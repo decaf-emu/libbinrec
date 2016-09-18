63ae95389d03c16a8186dbb3095da6bf8b5ab4de
Andrew Church <achurch@achurch.org>
2016-09-18 22:34:24 +0900

    Add missing test for using R12 as an EA base register.

---
+++ tests/host-x86/insn/load-int32-r12.c
@@ -0,0 +1,51 @@
+/*
+ * libbinrec: a recompiling translator for machine code
+ * Copyright (c) 2016 Andrew Church <achurch@achurch.org>
+ *
+ * This software may be copied and redistributed under certain conditions;
+ * see the file "COPYING" in the source code distribution for details.
+ * NO WARRANTY is provided with this software.
+ */
+
+#include "tests/common.h"
+#include "tests/host-x86/common.h"
+
+
+static const binrec_setup_t setup = {
+    .host = BINREC_ARCH_X86_64_SYSV,
+};
+static const unsigned int host_opt = 0;
+
+static int add_rtl(RTLUnit *unit)
+{
+    alloc_dummy_registers(unit, 11, RTLTYPE_INT32);
+
+    uint32_t reg1, reg2;
+    EXPECT(reg1 = rtl_alloc_register(unit, RTLTYPE_ADDRESS));
+    EXPECT(rtl_add_insn(unit, RTLOP_LOAD_IMM, reg1, 0, 0, 0));
+    EXPECT(reg2 = rtl_alloc_register(unit, RTLTYPE_INT32));
+    EXPECT(rtl_add_insn(unit, RTLOP_LOAD, reg2, reg1, 0, 0));
+    EXPECT(rtl_add_insn(unit, RTLOP_NOP, 0, reg1, 0, 0));
+
+    return EXIT_SUCCESS;
+}
+
+static const uint8_t expected_code[] = {
+    0x53,                               // push %rbx
+    0x55,                               // push %rbp
+    0x41,0x54,                          // push %r12
+    0x41,0x55,                          // push %r13
+    0x48,0x83,0xEC,0x08,                // sub $8,%rsp
+    0x45,0x33,0xE4,                     // xor %r12d,%r12d
+    0x45,0x8B,0x2C,0x24,                // mov (%r12),%r13d
+    0x48,0x83,0xC4,0x08,                // add $8,%rsp
+    0x41,0x5D,                          // pop %r13
+    0x41,0x5C,                          // pop %r12
+    0x5D,                               // pop %rbp
+    0x5B,                               // pop %rbx
+    0xC3,                               // ret
+};
+
+static const char expected_log[] = "";
+
+#include "tests/rtl-translate-test.i"
