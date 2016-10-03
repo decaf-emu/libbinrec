b96b5c25af8731ab65b02d719bbf1ee64f762b17
Andrew Church <achurch@achurch.org>
2016-10-04 01:10:13 +0900

    Improve test coverage.

---
+++ tests/guest-ppc/general/translate-block-oom.c
@@ -0,0 +1,86 @@
+/*
+ * libbinrec: a recompiling translator for machine code
+ * Copyright (c) 2016 Andrew Church <achurch@achurch.org>
+ *
+ * This software may be copied and redistributed under certain conditions;
+ * see the file "COPYING" in the source code distribution for details.
+ * NO WARRANTY is provided with this software.
+ */
+
+#include "src/guest-ppc.h"
+#include "src/rtl.h"
+#include "src/rtl-internal.h"
+#include "tests/common.h"
+#include "tests/mem-wrappers.h"
+
+
+int main(void)
+{
+    uint32_t illegal_insn = 0;
+
+    binrec_setup_t setup;
+    memset(&setup, 0, sizeof(setup));
+    setup.guest_memory_base = &illegal_insn;
+    setup.host_memory_base = UINT64_C(0x100000000);
+    setup.state_offset_gpr = 0x100;
+    setup.state_offset_fpr = 0x180;
+    setup.state_offset_gqr = 0x380;
+    setup.state_offset_cr = 0x3A0;
+    setup.state_offset_lr = 0x3A4;
+    setup.state_offset_ctr = 0x3A8;
+    setup.state_offset_xer = 0x3AC;
+    setup.state_offset_fpscr = 0x3B0;
+    setup.state_offset_reserve_flag = 0x3B4;
+    setup.state_offset_reserve_state = 0x3B8;
+    setup.state_offset_nia = 0x3BC;
+    setup.state_offset_timebase_handler = 0x3C8;
+    setup.state_offset_sc_handler = 0x3D0;
+    setup.state_offset_trap_handler = 0x3D8;
+    setup.malloc = mem_wrap_malloc;
+    setup.realloc = mem_wrap_realloc;
+    setup.free = mem_wrap_free;
+    binrec_t *handle;
+    EXPECT(handle = binrec_create_handle(&setup));
+
+    RTLUnit *unit;
+    for (int count = 1; ; count++) {
+        if (count >= 100) {
+            FAIL("Failed to translate code after 100 tries");
+        }
+        EXPECT(unit = rtl_create_unit(handle));
+        /* Adjust instruction array size to force reallocation (which will
+         * fail). */
+        unit->insns_size = count;
+        mem_wrap_fail_after(1);
+        if (guest_ppc_translate(handle, 0, 3, unit)) {
+            if (count == 1) {
+                FAIL("Translation did not fail on memory allocation failure");
+            }
+            break;
+        }
+        rtl_destroy_unit(unit);
+    }
+
+    mem_wrap_cancel_fail();
+    EXPECT_STREQ(rtl_disassemble_unit(unit, false),
+                 "    0: LOAD_ARG   r1, 0\n"
+                 "    1: LOAD_IMM   r2, 0x100000000\n"
+                 "    2: LABEL      L1\n"
+                 "    3: ILLEGAL\n"
+                 "    4: LOAD_IMM   r3, 4\n"
+                 "    5: SET_ALIAS  a1, r3\n"
+                 "    6: LABEL      L2\n"
+                 "    7: RETURN\n"
+                 "\n"
+                 "Alias 1: int32 @ 956(r1)\n"
+                 "\n"
+                 "Block 0: <none> --> [0,1] --> 1\n"
+                 "Block 1: 0 --> [2,5] --> 2\n"
+                 "Block 2: 1 --> [6,7] --> <none>\n"
+                 );
+
+
+    rtl_destroy_unit(unit);
+    binrec_destroy_handle(handle);
+    return EXIT_SUCCESS;
+}
--- tests/host-x86/general/alias-merge-full-entry-list.c
+++ tests/host-x86/general/alias-merge-full-entry-list.c
@@ -18,32 +18,39 @@ static const unsigned int host_opt = 0;
 
 static int add_rtl(RTLUnit *unit)
 {
-    int reg1, alias, label;
+    int reg1, alias1, alias2, label;
     EXPECT(reg1 = rtl_alloc_register(unit, RTLTYPE_ADDRESS));
     EXPECT(rtl_add_insn(unit, RTLOP_LOAD_ARG, reg1, 0, 0, 0));
-    EXPECT(alias = rtl_alloc_alias_register(unit, RTLTYPE_INT32));
-    rtl_set_alias_storage(unit, alias, reg1, 0x1234);
+    EXPECT(alias1 = rtl_alloc_alias_register(unit, RTLTYPE_INT32));
+    rtl_set_alias_storage(unit, alias1, reg1, 0x1234);
+    EXPECT(alias2 = rtl_alloc_alias_register(unit, RTLTYPE_INT32));
+    rtl_set_alias_storage(unit, alias2, reg1, 0x5678);
     EXPECT(label = rtl_alloc_label(unit));
 
     for (int i = 1; i <= 8; i++) {
         int regA, regB;
         EXPECT(regA = rtl_alloc_register(unit, RTLTYPE_INT32));
-        EXPECT(rtl_add_insn(unit, RTLOP_GET_ALIAS, regA, 0, 0, alias));
+        EXPECT(rtl_add_insn(unit, RTLOP_GET_ALIAS, regA, 0, 0, alias1));
         EXPECT(regB = rtl_alloc_register(unit, RTLTYPE_INT32));
         EXPECT(rtl_add_insn(unit, RTLOP_ADDI, regB, regA, 0, -1));
-        EXPECT(rtl_add_insn(unit, RTLOP_SET_ALIAS, 0, regB, 0, alias));
+        EXPECT(rtl_add_insn(unit, RTLOP_SET_ALIAS, 0, regB, 0, alias1));
         EXPECT(rtl_add_insn(unit, RTLOP_GOTO_IF_Z, 0, regB, 0, label));
     }
 
     EXPECT(rtl_add_insn(unit, RTLOP_RETURN, 0, 0, 0, 0));
 
-    int reg2, reg3;
+    int reg2, reg3, reg4, reg5;
     EXPECT(rtl_add_insn(unit, RTLOP_LABEL, 0, 0, 0, label));
     EXPECT(reg2 = rtl_alloc_register(unit, RTLTYPE_INT32));
-    EXPECT(rtl_add_insn(unit, RTLOP_GET_ALIAS, reg2, 0, 0, alias));
+    EXPECT(rtl_add_insn(unit, RTLOP_GET_ALIAS, reg2, 0, 0, alias1));
     EXPECT(reg3 = rtl_alloc_register(unit, RTLTYPE_INT32));
     EXPECT(rtl_add_insn(unit, RTLOP_SLLI, reg3, reg2, 0, 1));
-    EXPECT(rtl_add_insn(unit, RTLOP_SET_ALIAS, 0, reg3, 0, alias));
+    EXPECT(rtl_add_insn(unit, RTLOP_SET_ALIAS, 0, reg3, 0, alias1));
+    EXPECT(reg4 = rtl_alloc_register(unit, RTLTYPE_INT32));
+    EXPECT(rtl_add_insn(unit, RTLOP_GET_ALIAS, reg4, 0, 0, alias2));
+    EXPECT(reg5 = rtl_alloc_register(unit, RTLTYPE_INT32));
+    EXPECT(rtl_add_insn(unit, RTLOP_ADD, reg5, reg3, reg4, 1));
+    EXPECT(rtl_add_insn(unit, RTLOP_SET_ALIAS, 0, reg5, 0, alias2));
 
     EXPECT(rtl_add_insn(unit, RTLOP_NOP, 0, reg1, 0, 0));
     return EXIT_SUCCESS;
@@ -77,9 +84,12 @@ static const uint8_t expected_code[] = {
     0x89,0x87,0x34,0x12,0x00,0x00,      // mov %eax,0x1234(%rdi)
     0x85,0xC0,                          // test %eax,%eax
     0x0F,0x84,0x05,0x00,0x00,0x00,      // jz L1
-    0xE9,0x09,0x00,0x00,0x00,           // jmp L0
+    0xE9,0x17,0x00,0x00,0x00,           // jmp L0
     0xC1,0xE0,0x01,                     // L1: shl $1,%eax
     0x89,0x87,0x34,0x12,0x00,0x00,      // mov %eax,0x1234(%rdi)
+    0x8B,0x8F,0x78,0x56,0x00,0x00,      // mov 0x5678(%rdi),%ecx
+    0x03,0xC1,                          // add %ecx,%eax
+    0x89,0x87,0x78,0x56,0x00,0x00,      // mov %eax,0x5678(%rdi)
     0x48,0x83,0xC4,0x08,                // L0: add $8,%rsp
     0xC3,                               // ret
 };
