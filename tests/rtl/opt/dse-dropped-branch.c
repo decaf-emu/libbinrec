349f4573ea33f0c38c8577cd101c626f0acec034
Andrew Church <achurch@achurch.org>
2016-10-09 17:19:21 +0900

    Forgot to add this test.

---
+++ tests/rtl/opt/dse-dropped-branch.c
@@ -0,0 +1,44 @@
+/*
+ * libbinrec: a recompiling translator for machine code
+ * Copyright (c) 2016 Andrew Church <achurch@achurch.org>
+ *
+ * This software may be copied and redistributed under certain conditions;
+ * see the file "COPYING" in the source code distribution for details.
+ * NO WARRANTY is provided with this software.
+ */
+
+#include "src/rtl-internal.h"
+#include "tests/common.h"
+
+
+static unsigned int opt_flags = BINREC_OPT_BASIC | BINREC_OPT_DSE;
+
+static int add_rtl(RTLUnit *unit)
+{
+    int label, reg1;
+    EXPECT(label = rtl_alloc_label(unit));
+    EXPECT(reg1 = rtl_alloc_register(unit, RTLTYPE_INT32));
+    EXPECT(rtl_add_insn(unit, RTLOP_LOAD_ARG, reg1, 0, 0, 0));
+    EXPECT(rtl_add_insn(unit, RTLOP_GOTO_IF_Z, 0, reg1, 0, label));
+    EXPECT(rtl_add_insn(unit, RTLOP_LABEL, 0, 0, 0, label));
+    EXPECT(rtl_add_insn(unit, RTLOP_RETURN, 0, 0, 0, 0));
+
+    return EXIT_SUCCESS;
+}
+
+static const char expected[] =
+    #ifdef RTL_DEBUG_OPTIMIZE
+        "[info] Dropping branch at 1 to next insn\n"
+        "[info] r1 no longer used, setting death = birth\n"
+        "[info] Dropping dead store to r1 at insn 0\n"
+    #endif
+    "    0: NOP\n"
+    "    1: NOP\n"
+    "    2: LABEL      L1\n"
+    "    3: RETURN\n"
+    "\n"
+    "Block 0: <none> --> [0,1] --> 1\n"
+    "Block 1: 0 --> [2,3] --> <none>\n"
+    ;
+
+#include "tests/rtl-optimize-test.i"
