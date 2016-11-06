fe006d6e6bf3ac4a78f47677ad6dd49202fe9054
Andrew Church <achurch@achurch.org>
2016-11-07 00:47:56 +0900

    Document suboptimal register allocation which isn't important for now.

--- tests/host-x86/insn/set_alias-nontail-call.c
+++ tests/host-x86/insn/set_alias-nontail-call.c
@@ -44,6 +44,12 @@ static const uint8_t expected_code[] = {
     0x53,                               // push %rbx
     0x48,0x83,0xEC,0x10,                // sub $16,%rsp
     0xBB,0x01,0x00,0x00,0x00,           // mov $1,%ebx
+    /* The use of EAX here is suboptimal; this is because the register
+     * allocator sees reg2 dying on the CALL instruction (due to live
+     * range extension from aliases) and assumes the register is consumed
+     * by the CALL.  This only occurs if a nontail CALL is the last
+     * instruction in a block, which currently won't occur during actual
+     * translation, so we don't worry about it. */
     0xB8,0x02,0x00,0x00,0x00,           // mov $2,%eax
     0x89,0x83,0x34,0x12,0x00,0x00,      // mov %eax,0x1234(%rbx)
     0x48,0x89,0x04,0x24,                // mov %rax,(%rsp)
